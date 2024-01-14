50023c5b58b68f524503f43671670ebb7f049003
s0584574 <mekchedy@gmail.com>
2024-01-14 01:27:55 +0100

    add html creator and gitignore

--- utils/htmlemailcreator.go
+++ utils/htmlemailcreator.go
@@ -1,6 +1,15 @@
 package emailhtml
 
-func HtmlEmailCreator(){
+import( 
+	gomail "gopkg.in/gomail.v2"
+)
 
+func HtmlEmailCreator(to []string, from string, htmlData string, imagepath string){
+    msg := gomail.NewMessage()
+    msg.SetHeader("From", from)
+    msg.SetHeader("To", to)
+    msg.SetHeader("Subject", )
+    msg.SetBody("text/html", htmlData)
+    msg.Attach(imagepath)	
 
 }
